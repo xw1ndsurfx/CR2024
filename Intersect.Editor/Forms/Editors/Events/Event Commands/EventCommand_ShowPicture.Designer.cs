be976c910634834aced525655896038a89c0551d
Nicolas St-Onge <49542054+xw1ndsurfx@users.noreply.github.com>
2024-05-07 22:12:01 -0400

    Update Edtior UI image

--- Intersect.Editor/Forms/Editors/Events/Event Commands/EventCommand_ShowPicture.Designer.cs
+++ Intersect.Editor/Forms/Editors/Events/Event Commands/EventCommand_ShowPicture.Designer.cs
@@ -28,186 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.grpShowPicture = new DarkUI.Controls.DarkGroupBox();
-            this.chkClick = new DarkUI.Controls.DarkCheckBox();
-            this.lblSize = new System.Windows.Forms.Label();
-            this.lblPicture = new System.Windows.Forms.Label();
-            this.btnCancel = new DarkUI.Controls.DarkButton();
-            this.btnSave = new DarkUI.Controls.DarkButton();
-            this.lblHide = new System.Windows.Forms.Label();
-            this.cmbSize = new DarkUI.Controls.DarkComboBox();
-            this.cmbPicture = new DarkUI.Controls.DarkComboBox();
-            this.nudHideTime = new DarkUI.Controls.DarkNumericUpDown();
-            this.chkWaitUntilClosed = new DarkUI.Controls.DarkCheckBox();
-            this.grpShowPicture.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nudHideTime)).BeginInit();
-            this.SuspendLayout();
+            grpShowPicture = new DarkUI.Controls.DarkGroupBox();
+            chkWaitUntilClosed = new DarkUI.Controls.DarkCheckBox();
+            nudHideTime = new DarkUI.Controls.DarkNumericUpDown();
+            lblHide = new Label();
+            chkClick = new DarkUI.Controls.DarkCheckBox();
+            cmbSize = new DarkUI.Controls.DarkComboBox();
+            lblSize = new Label();
+            cmbPicture = new DarkUI.Controls.DarkComboBox();
+            lblPicture = new Label();
+            btnCancel = new DarkUI.Controls.DarkButton();
+            btnSave = new DarkUI.Controls.DarkButton();
+            grpShowPicture.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudHideTime).BeginInit();
+            SuspendLayout();
             // 
             // grpShowPicture
             // 
-            this.grpShowPicture.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
-            this.grpShowPicture.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
-            this.grpShowPicture.Controls.Add(this.chkWaitUntilClosed);
-            this.grpShowPicture.Controls.Add(this.nudHideTime);
-            this.grpShowPicture.Controls.Add(this.lblHide);
-            this.grpShowPicture.Controls.Add(this.chkClick);
-            this.grpShowPicture.Controls.Add(this.cmbSize);
-            this.grpShowPicture.Controls.Add(this.lblSize);
-            this.grpShowPicture.Controls.Add(this.cmbPicture);
-            this.grpShowPicture.Controls.Add(this.lblPicture);
-            this.grpShowPicture.Controls.Add(this.btnCancel);
-            this.grpShowPicture.Controls.Add(this.btnSave);
-            this.grpShowPicture.ForeColor = System.Drawing.Color.Gainsboro;
-            this.grpShowPicture.Location = new System.Drawing.Point(3, 3);
-            this.grpShowPicture.Name = "grpShowPicture";
-            this.grpShowPicture.Size = new System.Drawing.Size(248, 195);
-            this.grpShowPicture.TabIndex = 18;
-            this.grpShowPicture.TabStop = false;
-            this.grpShowPicture.Text = "Show Picture:";
-            // 
-            // chkClick
-            // 
-            this.chkClick.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.chkClick.Location = new System.Drawing.Point(93, 110);
-            this.chkClick.Name = "chkClick";
-            this.chkClick.Size = new System.Drawing.Size(149, 17);
-            this.chkClick.TabIndex = 25;
-            this.chkClick.Text = "Click To Close?";
-            // 
-            // lblSize
-            // 
-            this.lblSize.AutoSize = true;
-            this.lblSize.Location = new System.Drawing.Point(5, 53);
-            this.lblSize.Name = "lblSize";
-            this.lblSize.Size = new System.Drawing.Size(30, 13);
-            this.lblSize.TabIndex = 23;
-            this.lblSize.Text = "Size:";
-            // 
-            // lblPicture
-            // 
-            this.lblPicture.AutoSize = true;
-            this.lblPicture.Location = new System.Drawing.Point(4, 22);
-            this.lblPicture.Name = "lblPicture";
-            this.lblPicture.Size = new System.Drawing.Size(43, 13);
-            this.lblPicture.TabIndex = 21;
-            this.lblPicture.Text = "Picture:";
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Location = new System.Drawing.Point(167, 165);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Padding = new System.Windows.Forms.Padding(5);
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
-            this.btnCancel.TabIndex = 20;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-            // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(6, 165);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Padding = new System.Windows.Forms.Padding(5);
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
-            this.btnSave.TabIndex = 19;
-            this.btnSave.Text = "Ok";
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // lblHide
-            // 
-            this.lblHide.AutoSize = true;
-            this.lblHide.Location = new System.Drawing.Point(5, 84);
-            this.lblHide.Name = "lblHide";
-            this.lblHide.Size = new System.Drawing.Size(79, 13);
-            this.lblHide.TabIndex = 26;
-            this.lblHide.Text = "Hide After (ms):";
-            // 
-            // cmbSize
-            // 
-            this.cmbSize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
-            this.cmbSize.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
-            this.cmbSize.BorderStyle = System.Windows.Forms.ButtonBorderStyle.Solid;
-            this.cmbSize.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
-            this.cmbSize.DrawDropdownHoverOutline = false;
-            this.cmbSize.DrawFocusRectangle = false;
-            this.cmbSize.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.cmbSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbSize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cmbSize.ForeColor = System.Drawing.Color.Gainsboro;
-            this.cmbSize.FormattingEnabled = true;
-            this.cmbSize.Items.AddRange(new object[] {
-            "Original",
-            "Full Screen",
-            "Half Screen",
-            "Stretch To Fit"});
-            this.cmbSize.Location = new System.Drawing.Point(93, 53);
-            this.cmbSize.Name = "cmbSize";
-            this.cmbSize.Size = new System.Drawing.Size(149, 21);
-            this.cmbSize.TabIndex = 24;
-            this.cmbSize.Text = "Original";
-            this.cmbSize.TextPadding = new System.Windows.Forms.Padding(2);
-            // 
-            // cmbPicture
-            // 
-            this.cmbPicture.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
-            this.cmbPicture.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
-            this.cmbPicture.BorderStyle = System.Windows.Forms.ButtonBorderStyle.Solid;
-            this.cmbPicture.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
-            this.cmbPicture.DrawDropdownHoverOutline = false;
-            this.cmbPicture.DrawFocusRectangle = false;
-            this.cmbPicture.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.cmbPicture.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbPicture.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cmbPicture.ForeColor = System.Drawing.Color.Gainsboro;
-            this.cmbPicture.FormattingEnabled = true;
-            this.cmbPicture.Location = new System.Drawing.Point(93, 22);
-            this.cmbPicture.Name = "cmbPicture";
-            this.cmbPicture.Size = new System.Drawing.Size(149, 21);
-            this.cmbPicture.TabIndex = 22;
-            this.cmbPicture.Text = null;
-            this.cmbPicture.TextPadding = new System.Windows.Forms.Padding(2);
-            // 
-            // nudHideTime
-            // 
-            this.nudHideTime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
-            this.nudHideTime.ForeColor = System.Drawing.Color.Gainsboro;
-            this.nudHideTime.Location = new System.Drawing.Point(93, 84);
-            this.nudHideTime.Maximum = new decimal(new int[] {
-            100000000,
-            0,
-            0,
-            0});
-            this.nudHideTime.Name = "nudHideTime";
-            this.nudHideTime.Size = new System.Drawing.Size(149, 20);
-            this.nudHideTime.TabIndex = 27;
-            this.nudHideTime.Value = new decimal(new int[] {
-            0,
-            0,
-            0,
-            0});
+            grpShowPicture.BackColor = System.Drawing.Color.FromArgb(60, 63, 65);
+            grpShowPicture.BorderColor = System.Drawing.Color.FromArgb(90, 90, 90);
+            grpShowPicture.Controls.Add(chkWaitUntilClosed);
+            grpShowPicture.Controls.Add(nudHideTime);
+            grpShowPicture.Controls.Add(lblHide);
+            grpShowPicture.Controls.Add(chkClick);
+            grpShowPicture.Controls.Add(cmbSize);
+            grpShowPicture.Controls.Add(lblSize);
+            grpShowPicture.Controls.Add(cmbPicture);
+            grpShowPicture.Controls.Add(lblPicture);
+            grpShowPicture.Controls.Add(btnCancel);
+            grpShowPicture.Controls.Add(btnSave);
+            grpShowPicture.ForeColor = System.Drawing.Color.Gainsboro;
+            grpShowPicture.Location = new System.Drawing.Point(4, 3);
+            grpShowPicture.Margin = new Padding(4, 3, 4, 3);
+            grpShowPicture.Name = "grpShowPicture";
+            grpShowPicture.Padding = new Padding(4, 3, 4, 3);
+            grpShowPicture.Size = new Size(416, 225);
+            grpShowPicture.TabIndex = 18;
+            grpShowPicture.TabStop = false;
+            grpShowPicture.Text = "Show Picture:";
             // 
             // chkWaitUntilClosed
             // 
-            this.chkWaitUntilClosed.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.chkWaitUntilClosed.Location = new System.Drawing.Point(93, 133);
-            this.chkWaitUntilClosed.Name = "chkWaitUntilClosed";
-            this.chkWaitUntilClosed.Size = new System.Drawing.Size(149, 17);
-            this.chkWaitUntilClosed.TabIndex = 28;
-            this.chkWaitUntilClosed.Text = "Wait Until Closed?";
+            chkWaitUntilClosed.CheckAlign = ContentAlignment.MiddleRight;
+            chkWaitUntilClosed.Location = new System.Drawing.Point(108, 153);
+            chkWaitUntilClosed.Margin = new Padding(4, 3, 4, 3);
+            chkWaitUntilClosed.Name = "chkWaitUntilClosed";
+            chkWaitUntilClosed.Size = new Size(174, 20);
+            chkWaitUntilClosed.TabIndex = 28;
+            chkWaitUntilClosed.Text = "Wait Until Closed?";
+            // 
+            // nudHideTime
+            // 
+            nudHideTime.BackColor = System.Drawing.Color.FromArgb(69, 73, 74);
+            nudHideTime.ForeColor = System.Drawing.Color.Gainsboro;
+            nudHideTime.Location = new System.Drawing.Point(108, 97);
+            nudHideTime.Margin = new Padding(4, 3, 4, 3);
+            nudHideTime.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
+            nudHideTime.Name = "nudHideTime";
+            nudHideTime.Size = new Size(174, 23);
+            nudHideTime.TabIndex = 27;
+            nudHideTime.Value = new decimal(new int[] { 0, 0, 0, 0 });
+            // 
+            // lblHide
+            // 
+            lblHide.AutoSize = true;
+            lblHide.Location = new System.Drawing.Point(6, 97);
+            lblHide.Margin = new Padding(4, 0, 4, 0);
+            lblHide.Name = "lblHide";
+            lblHide.Size = new Size(91, 15);
+            lblHide.TabIndex = 26;
+            lblHide.Text = "Hide After (ms):";
+            // 
+            // chkClick
+            // 
+            chkClick.CheckAlign = ContentAlignment.MiddleRight;
+            chkClick.Location = new System.Drawing.Point(108, 127);
+            chkClick.Margin = new Padding(4, 3, 4, 3);
+            chkClick.Name = "chkClick";
+            chkClick.Size = new Size(174, 20);
+            chkClick.TabIndex = 25;
+            chkClick.Text = "Click To Close?";
+            // 
+            // cmbSize
+            // 
+            cmbSize.BackColor = System.Drawing.Color.FromArgb(69, 73, 74);
+            cmbSize.BorderColor = System.Drawing.Color.FromArgb(90, 90, 90);
+            cmbSize.BorderStyle = ButtonBorderStyle.Solid;
+            cmbSize.ButtonColor = System.Drawing.Color.FromArgb(43, 43, 43);
+            cmbSize.DrawDropdownHoverOutline = false;
+            cmbSize.DrawFocusRectangle = false;
+            cmbSize.DrawMode = DrawMode.OwnerDrawFixed;
+            cmbSize.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbSize.FlatStyle = FlatStyle.Flat;
+            cmbSize.ForeColor = System.Drawing.Color.Gainsboro;
+            cmbSize.FormattingEnabled = true;
+            cmbSize.Items.AddRange(new object[] { "Original", "Full Screen", "Half Screen", "Stretch To Fit" });
+            cmbSize.Location = new System.Drawing.Point(108, 61);
+            cmbSize.Margin = new Padding(4, 3, 4, 3);
+            cmbSize.Name = "cmbSize";
+            cmbSize.Size = new Size(173, 24);
+            cmbSize.TabIndex = 24;
+            cmbSize.Text = "Original";
+            cmbSize.TextPadding = new Padding(2);
+            // 
+            // lblSize
+            // 
+            lblSize.AutoSize = true;
+            lblSize.Location = new System.Drawing.Point(6, 61);
+            lblSize.Margin = new Padding(4, 0, 4, 0);
+            lblSize.Name = "lblSize";
+            lblSize.Size = new Size(30, 15);
+            lblSize.TabIndex = 23;
+            lblSize.Text = "Size:";
+            // 
+            // cmbPicture
+            // 
+            cmbPicture.BackColor = System.Drawing.Color.FromArgb(69, 73, 74);
+            cmbPicture.BorderColor = System.Drawing.Color.FromArgb(90, 90, 90);
+            cmbPicture.BorderStyle = ButtonBorderStyle.Solid;
+            cmbPicture.ButtonColor = System.Drawing.Color.FromArgb(43, 43, 43);
+            cmbPicture.Dock = DockStyle.Top;
+            cmbPicture.DrawDropdownHoverOutline = false;
+            cmbPicture.DrawFocusRectangle = false;
+            cmbPicture.DrawMode = DrawMode.OwnerDrawFixed;
+            cmbPicture.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbPicture.FlatStyle = FlatStyle.Flat;
+            cmbPicture.ForeColor = System.Drawing.Color.Gainsboro;
+            cmbPicture.FormattingEnabled = true;
+            cmbPicture.Location = new System.Drawing.Point(4, 19);
+            cmbPicture.Margin = new Padding(4, 3, 4, 3);
+            cmbPicture.Name = "cmbPicture";
+            cmbPicture.Size = new Size(408, 24);
+            cmbPicture.TabIndex = 22;
+            cmbPicture.Text = null;
+            cmbPicture.TextPadding = new Padding(2);
+            // 
+            // lblPicture
+            // 
+            lblPicture.AutoSize = true;
+            lblPicture.Location = new System.Drawing.Point(44, 46);
+            lblPicture.Margin = new Padding(4, 0, 4, 0);
+            lblPicture.Name = "lblPicture";
+            lblPicture.Size = new Size(47, 15);
+            lblPicture.TabIndex = 21;
+            lblPicture.Text = "Picture:";
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new System.Drawing.Point(195, 190);
+            btnCancel.Margin = new Padding(4, 3, 4, 3);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Padding = new Padding(6, 6, 6, 6);
+            btnCancel.Size = new Size(88, 27);
+            btnCancel.TabIndex = 20;
+            btnCancel.Text = "Cancel";
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new System.Drawing.Point(7, 190);
+            btnSave.Margin = new Padding(4, 3, 4, 3);
+            btnSave.Name = "btnSave";
+            btnSave.Padding = new Padding(6, 6, 6, 6);
+            btnSave.Size = new Size(88, 27);
+            btnSave.TabIndex = 19;
+            btnSave.Text = "Ok";
+            btnSave.Click += btnSave_Click;
             // 
             // EventCommand_ShowPicture
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
-            this.Controls.Add(this.grpShowPicture);
-            this.Name = "EventCommand_ShowPicture";
-            this.Size = new System.Drawing.Size(254, 201);
-            this.grpShowPicture.ResumeLayout(false);
-            this.grpShowPicture.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nudHideTime)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = System.Drawing.Color.FromArgb(45, 45, 48);
+            Controls.Add(grpShowPicture);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "EventCommand_ShowPicture";
+            Size = new Size(424, 232);
+            grpShowPicture.ResumeLayout(false);
+            grpShowPicture.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nudHideTime).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
